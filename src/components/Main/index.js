4d3922373cb6f487cf6b8bcd5c4fe8552f20cff9
Robert Adams <adamsiii.robert@gmail.com>
2021-05-22 16:12:44 -0700

    first draft, must make project card components

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// TODO: Refactor these sections into components
+// This component should be a class that stores the info of each project
+// pass that info into the cards as props
 export default function index() {
     return (
         <main>
@@ -9,13 +12,17 @@ export default function index() {
                 </h2>
             </section>
 
-            <section id="about" class="main-box">
-                <h1 class="main-heading-style">
+            {/* make a main-box component */}
+            <section id="about" className="main-box">
+
+                {/* make this a component */}
+                <h1 className="main-heading-style">
                     About Me
                 </h1>
-                <section class="content-box">
+                
+                <section className="content-box">
                     <p>
-                        Hello! I am learning full-stack web development through the University of Washington in partnership with Trilogy Education Services.
+                        Hello! I am learning full-stack web development through the University of Washington in partnership with Trilogy Education Services. 
                     </p>
                     <p>
                         Check out some of my recent projects below!
@@ -23,79 +30,110 @@ export default function index() {
                 </section>
             </section>
 
-            <section id="work" class="main-box">
-            <h1 class="main-heading-style">
+            {/* make a main-box component */}
+            <section id="work" className="main-box">
+
+            {/* make this a component */}
+            <h1 className="main-heading-style">
                 Work
             </h1>
 
-            <section class="content-box">
+            {/* make a content box component for the project cards */}
+            <section className="content-box">
 
-                <section id="featured" class="project-card">
-                    <a href="https://gushihiro.github.io/Project-Face-Space/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/Gushihiro/Project-Face-Space/develop/assets/images/vedomy.PNG" alt="front page of Vědomý webapp"/></a>
-                    <section class="title-card">
-                        <h2 class="title-card-heading">
-                            Featured: Vědomý
+                <section className="project-card">
+                    <a href="https://gushihiro.github.io/Vedomy/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/Gushihiro/Project-Face-Space/develop/assets/images/vedomy.PNG" alt="front page of Vědomý webapp"/></a>
+                    <section className="title-card">
+                        <h2 className="title-card-heading">
+                            Vědomý
                         </h2>
-                        <h3 class="title-card-lang">
+                        <h3 className="title-card-lang">
                             HTML/CSS/JavaScript
                         </h3>
-                        <h3 class="title-card-lang">
-                            <a class="link-color" href="https://github.com/Gushihiro/Project-Face-Space" target="_blank" rel="noreferrer">GitHub</a>
+                        <h3 className="title-card-lang">
+                            <a className="link-color" href="https://github.com/Gushihiro/Project-Face-Space" target="_blank" rel="noreferrer">GitHub</a>
                         </h3>
                     </section>
                 </section>
 
-                <section class="project-card">
-                    <a href="https://pantrimonium.herokuapp.com/"><img src="https://raw.githubusercontent.com/comatosino/portfolio/main/Assets/images/pantrimonium-screenshot.jpg" alt="pantrimonium"/></a>
-
-                    <section class="title-card">
-                        <h2 class="title-card-heading">
+                <section className="project-card">
+                    <a href="https://pantrimonium.herokuapp.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/comatosino/portfolio/main/Assets/images/pantrimonium-screenshot.jpg" alt="pantrimonium"/></a>
+                    <section className="title-card">
+                        <h2 className="title-card-heading">
                             Pantrimonium
                         </h2>
-                        <h3 class="title-card-lang">
+                        <h3 className="title-card-lang">
                             Express.js/Sequelize/Handlebars.js
                         </h3>
-                        <h3 class="title-card-lang">
-                            <a class="link-color" href="https://github.com/comatosino/Pantrimonium" target="_blank" rel="noreferrer">GitHub</a>
+                        <h3 className="title-card-lang">
+                            <a className="link-color" href="https://github.com/comatosino/Pantrimonium" target="_blank" rel="noreferrer">GitHub</a>
                         </h3>
                     </section>
                 </section>
 
-                <section class="project-card">
-                    <a href="https://github.com/comatosino/e-commerce-back-end"><img src="https://raw.githubusercontent.com/comatosino/e-commerce-back-end/develop/assets/images/e-commerce-backend.gif" alt="e-commerce back-end"/></a>
-
-                    <section class="title-card">
-                        <h2 class="title-card-heading">
+                <section className="project-card">
+                    <a href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/comatosino/e-commerce-back-end/develop/assets/images/e-commerce-backend.gif" alt="e-commerce back-end"/></a>
+                    <section className="title-card">
+                        <h2 className="title-card-heading">
                             E-Commerce Back-End
                         </h2>
-                        <h3 class="title-card-lang">
+                        <h3 className="title-card-lang">
                             Express.js/Sequelize
                         </h3>
-                        <h3 class="title-card-lang">
-                            <a class="link-color" href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer">GitHub</a>
+                        <h3 className="title-card-lang">
+                            <a className="link-color" href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer">GitHub</a>
                         </h3>
                     </section>
                 </section>
+
+                <section className="project-card">
+                    <a href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/comatosino/e-commerce-back-end/develop/assets/images/e-commerce-backend.gif" alt="e-commerce back-end"/></a>
+                    <section className="title-card">
+                        <h2 className="title-card-heading">
+                            E-Commerce Back-End
+                        </h2>
+                        <h3 className="title-card-lang">
+                            Express.js/Sequelize
+                        </h3>
+                        <h3 className="title-card-lang">
+                            <a className="link-color" href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer">GitHub</a>
+                        </h3>
+                    </section>
+                </section>
+
+                <section className="project-card">
+                    <a href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/comatosino/e-commerce-back-end/develop/assets/images/e-commerce-backend.gif" alt="e-commerce back-end"/></a>
+                    <section className="title-card">
+                        <h2 className="title-card-heading">
+                            E-Commerce Back-End
+                        </h2>
+                        <h3 className="title-card-lang">
+                            Express.js/Sequelize
+                        </h3>
+                        <h3 className="title-card-lang">
+                            <a className="link-color" href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer">GitHub</a>
+                        </h3>
+                    </section>
+                </section>
+
+                <section className="project-card">
+                    <a href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/comatosino/e-commerce-back-end/develop/assets/images/e-commerce-backend.gif" alt="e-commerce back-end"/></a>
+                    <section className="title-card">
+                        <h2 className="title-card-heading">
+                            E-Commerce Back-End
+                        </h2>
+                        <h3 className="title-card-lang">
+                            Express.js/Sequelize
+                        </h3>
+                        <h3 className="title-card-lang">
+                            <a className="link-color" href="https://github.com/comatosino/e-commerce-back-end" target="_blank" rel="noreferrer">GitHub</a>
+                        </h3>
+                    </section>
+                </section>
+
             </section>
+
         </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </main>
+    </main>
     )
 }
