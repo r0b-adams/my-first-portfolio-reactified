fbe13b4edf85a4a28d72d1b5f34e302ed1eb2c4b
Robert Adams <adamsiii.robert@gmail.com>
2021-05-22 19:10:30 -0700

    added fix comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Footer from "./components/Footer/index";
 import "./reset.css";
 import "./style.css";
 
+// fixed deploy error:
+// git remote set-url origin https://github.com/USERNAME/REPOSITORY_NAME.git
+
 function App() {
   return (
     <div>
