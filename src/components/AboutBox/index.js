8ffce98e09280a2039e1101c0e985e4a430be66e
Robert Adams <adamsiii.robert@gmail.com>
2021-05-23 21:13:54 -0700

    added component

---
+++ src/components/AboutBox/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <div>
+            <p>
+                Hello! I am learning full-stack web development through the University of Washington in partnership with Trilogy Education Services. I couldn't be more excited to see what's next as I learn programming!
+            </p>
+            <p>
+                Check out some of my recent projects below!
+            </p>
+        </div>
+    )
+}
