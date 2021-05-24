2930b39b36d7bb573eba42c26e6355228110c65b
Robert Adams <adamsiii.robert@gmail.com>
2021-05-23 21:18:35 -0700

    added Subtitle component

---
+++ src/components/Subtitle/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Subtitle() {
+    return (
+        <section id="sub-img">
+            <h2 id="subtitle">
+                Welcome to my portfolio page! I'm learning how to code!
+            </h2>
+        </section>
+    )
+}
