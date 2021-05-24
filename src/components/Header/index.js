c15de17bb91d73c6735386d776779606957ac2ad
Robert Adams <adamsiii.robert@gmail.com>
2021-05-23 21:10:11 -0700

    added SectionHeading component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
+import SectionHeading from '../SectionHeading';
 import resume from './Adams_Robert_Resume.pdf'; // tells webpack to use this file
 
 export default function Header() {
 
     return (
             <header className="header-footer-style">
-                <h1 id="my-name" className="main-heading-style">
-                    Robert Adams
-                </h1>
+
+                <SectionHeading title="Robert Adams"/>
 
                 <nav>
-                    <ul class="nav-ul-style">
-                        <li>
-                            <a class="link-color" href="#about">About Me</a>
+                    <ul className="nav-ul-style">
+                        <li className="link-color">
+                            <a href="#about">About Me</a>
                         </li>
-                        <li>
-                            <a class="link-color" href="#work">Work</a>
+                        <li className="link-color">
+                            <a href="#work">Work</a>
                         </li>
-                        <li>
-                            <a class="link-color" href="#contact">Contact</a>
+                        <li className="link-color">
+                            <a href="#contact">Contact</a>
                         </li>
-                        <li>
-                            {/* set href to static file path */}
-                            {/* refer to static file like a variable */}
-                            <a class="link-color" href={resume}>Resume</a>
+                        <li className="link-color">
+                            <a href={resume}>Resume</a>
                         </li>
                     </ul>
                 </nav>
