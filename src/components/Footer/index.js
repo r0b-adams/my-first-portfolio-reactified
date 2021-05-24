43d5f418da7ea828ef5d7023c8780ee96b5238ec
Robert Adams <adamsiii.robert@gmail.com>
2021-05-23 21:09:10 -0700

    added SectionHeading component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from 'react';
+import SectionHeading from '../SectionHeading';
 
 export default function index() {
     return (
-        <footer id="contact" className="header-footer-style">
-        <h1 className="main-heading-style">
-            Contact Me
-        </h1>
-        <nav>
-            <ul className="nav-ul-style">
-                <li className="link-color">
-                    907.947.8558
-                </li>
-                <li className="link-color">
-                    <a href="mailto:adamsiii.robert@gmail.com">adamsiii.robert@gmail.com</a>
-                </li>
-                <li>
-                    <a className="link-color" href="https://github.com/comatosino" target="_blank" rel="noreferrer">GitHub</a>
-                </li>
-                <li>
-                    <a className="link-color" href="https://www.linkedin.com/in/robert-adams-comatosino/" target="_blank" rel="noreferrer">LinkedIn</a>
-                </li>
-            </ul>
-        </nav>
+        <footer className="header-footer-style">
+
+            <SectionHeading title="Contact Me" id="contact"/>
+
+            <nav>
+                <ul className="nav-ul-style">
+                    <li className="link-color">
+                        907.947.8558
+                    </li>
+                    <li className="link-color">
+                        <a href="mailto:adamsiii.robert@gmail.com">adamsiii.robert@gmail.com</a>
+                    </li>
+                    <li className="link-color">
+                        <a href="https://github.com/comatosino" target="_blank" rel="noreferrer">GitHub</a>
+                    </li>
+                    <li className="link-color">
+                        <a href="https://www.linkedin.com/in/robert-adams-comatosino/" target="_blank" rel="noreferrer">LinkedIn</a>
+                    </li>
+                </ul>
+            </nav>
+
     </footer>
     )
 }
