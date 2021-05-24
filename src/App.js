72a32f0f73b998ca392bcf8b77aed00446683e17
Robert Adams <adamsiii.robert@gmail.com>
2021-05-23 21:06:23 -0700

    moved style links to root /src/index.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Header from "./components/Header/index";
 import Main from "./components/Main/index";
 import Footer from "./components/Footer/index";
 
-import "./reset.css";
-import "./style.css";
-
 // fixed deploy error:
 // git remote set-url origin https://github.com/USERNAME/REPOSITORY_NAME.git
 
