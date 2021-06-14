ee7edba0a0c3b425d9e6d8e81e31ed5c21158ada
Robert Adams <adamsiii.robert@gmail.com>
2021-06-14 13:15:43 -0700

    tweaked about men box

--- src/components/AboutBox/index.js
+++ src/components/AboutBox/index.js
@@ -4,7 +4,7 @@ export default function index() {
     return (
         <div>
             <p>
-                Hello, I'm Robert! I'm a recent graduate of the University of Washington Coding Bootcamp! I'm just getting started and I can't wait to see what's next as I develop my skills and take the exciting step into a career in tech!
+                Hello, I'm Robert! I've recently received my certificate in full-stack web development. I'm just getting started and I can't wait to see what's next as I develop my skills and take the exciting step into a career in tech!
             </p>
             <p>
                 Check out some of my recent projects below!
